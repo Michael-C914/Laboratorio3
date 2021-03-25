eec43719a47924c7c967e836200e06e1f0656920
Michael <michael.cahuana@tecsup.edu.pe>
2021-03-24 23:45:48 -0500

    punto cinco

--- src/Total.js
+++ src/Total.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
-const Total = (props) => {
+
+const Total = ({parts}) => {
+  const amount = (item) => {
+    return item.exercises
+  }
+
+  const sum = (prev, next) => {
+    return prev + next
+  }
+
+  const total = parts.map(amount).reduce(sum)
   return(
-    <>
-      <p>Number of exercises {props.total}</p>
-    </>
+    <div>
+	    {parts.map}
+	      <p>Number of exercises {total} </p>
+    </div>  
   )
-  const {part1, part2, part3} = props
-  	const total = part1.exercises + part2.exercises + part3.exercises
-  	return(
-	    <div>
-	    	<p>Number of exercises {total}</p>
-	    </div>  
-  	)
 }
 
 export default Total 
